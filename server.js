b76f4efa2a0484573bdcc478520a2b5d7373116f
s-infinity21 <83367007+s-infinity21@users.noreply.github.com>
2021-06-07 17:23:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -53,8 +53,8 @@ io.on('connection', socket => {
 	});
 });
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-	console.log('Server Loaded on port 3000!');
+	console.log(`Server Loaded on port ${PORT}`);
 });
