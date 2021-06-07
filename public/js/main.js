52a5d32c0b90eedaa1510d83ff5579f4ee1986d4
s-infinity21 <83367007+s-infinity21@users.noreply.github.com>
2021-06-07 16:49:36 +0530

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,7 @@ const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
 const roomName = document.getElementById('room-name');
 const userArr = document.getElementById('users');
+var audio = new Audio('ding-sound-effect_2.mp3');
 
 const { username, room } = Qs.parse(location.search, {
 	ignoreQueryPrefix: true
@@ -26,6 +27,7 @@ socket.on('message', message => {
 	chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
+// Message submit
 chatForm.addEventListener('submit', event => {
 	event.preventDefault();
 	const msg = event.target.elements.msg.value;
@@ -34,10 +36,12 @@ chatForm.addEventListener('submit', event => {
 	event.target.elements.msg.focus();
 });
 
+// Output message to DOM
 function outputMessage(message) {
 	const div = document.createElement('div');
 	div.classList.add('message');
 	div.innerHTML = `<p class="meta">${message.username} <span> ${message.time} </span> </p> <p class="text"> ${message.text} </p>`;
+	audio.play();
 	document.querySelector('.chat-messages').appendChild(div);
 }
 
